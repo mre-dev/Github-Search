4ad30d24d6e0ed87b83881a6edd769f60f5f867e
mre-dev <mre.dev01@gmail.com>
2022-03-15 12:33:17 +0330

    Create UserCard Component

---
+++ src/components/UserCard/UserCard.component.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Styles from "assets/styles/components/UserCard/UserCard.module.css";
+
+export const UserCard = (props) => {
+    return (
+        <></>
+    );
+};
+
+UserCard.propTypes = {
+    
+};

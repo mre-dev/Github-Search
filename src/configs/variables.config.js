c8dcc85434b67301a879f87881ba0deec029573f
mre-dev <mre.dev01@gmail.com>
2022-03-15 20:39:55 +0330

    Add API URL

--- src/configs/variables.config.js
+++ src/configs/variables.config.js
@@ -1,2 +1,3 @@
 export const APP_ENVIORMENT = process.env.REACT_APP_APP_ENVIORMENT || 'development';
 export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
+export const BASE_API = process.env.REACT_APP_BASE_API || 'https://api.github.com/search/users?q=';

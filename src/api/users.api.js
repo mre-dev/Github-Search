bb41d90a02e896944fd3e5d0120743ac436b19c8
mre-dev <mre.dev01@gmail.com>
2022-03-16 01:31:26 +0330

    Create To APIs for Get User Information and Get User Repositories

--- src/api/users.api.js
+++ src/api/users.api.js
@@ -1,9 +1,27 @@
-import { BASE_API } from "configs/variables.config";
+import { BASE_API, USER_DETAILS_API } from "configs/variables.config";
 import httpService from "services/http.service";
 
 export async function GetUsers(userName) {
     try {
         const response = await httpService.get(BASE_API + userName);
+        return response;
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
+
+export async function GetUserDetails(userName) {
+    try {
+        const response = await httpService.get(USER_DETAILS_API + userName);
+        return response;
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
+
+export async function GetUserRepos(userName) {
+    try {
+        const response = await httpService.get(USER_DETAILS_API + userName + "/repos");
         return response;
     } catch (e) {
         return Promise.reject(e);

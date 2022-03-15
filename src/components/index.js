e30711844f34721ed66fa89c38f006237353c307
mre-dev <mre.dev01@gmail.com>
2022-03-15 13:36:21 +0330

    Update UserCard Component

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { Button } from "./Button/Button.component";
+export { Input } from "./Input/Input.component";
+export { Navigation } from "./Navigation/Navigation.component";
+export { UserCard } from "./UserCard/UserCard.component";

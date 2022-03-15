3e33f4232670ac7e953cef4c7845011b7c10503b
mre-dev <mre.dev01@gmail.com>
2022-03-16 01:59:52 +0330

    Create Not Found Page

--- src/pages/NotFound/NotFound.page.js
+++ src/pages/NotFound/NotFound.page.js
@@ -1,10 +1,24 @@
+import { Navigation } from 'components';
 import { Header } from 'layouts';
 import React from 'react';
 
 export const NotFoundPage = (props) => {
+
     return (
         <div>
             <Header title="Github Search" />
+            <div style={{
+                fontSize: '3.2rem',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                textShadow: '0rem 0rem 1rem black',
+                textAlign: 'center'
+            }}>
+                <p>Page Not Found</p>
+                <Navigation link='/' text='Go Home' internal/>
+            </div>
         </div>
     );
 };

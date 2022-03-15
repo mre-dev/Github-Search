d8673e79ce56b803b2f18b6ee6f25c62ab828f9f
mre-dev <mre.dev01@gmail.com>
2022-03-15 13:37:32 +0330

    Create About Page

--- src/pages/About/About.page.js
+++ src/pages/About/About.page.js
@@ -1,11 +1,18 @@
+import { Header } from 'layouts';
 import React from 'react';
-
+import Styles from './About.page.module.css';
 
 export const AboutPage = (props) => {
 
     return (
         <div>
-            About Page
+            <Header title="Github Search" />
+
+            <div className={Styles.container}>
+                <h2>About Page</h2>
+                <p>This app is github search</p>
+            </div>
+
         </div>
     );
 };

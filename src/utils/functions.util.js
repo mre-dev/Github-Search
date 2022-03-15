057d4a17b5c0f1efc1dd7881a16ee902bc9059d5
mre-dev <mre.dev01@gmail.com>
2022-03-15 21:03:05 +0330

    Create Functions for Save And Load Data From LocalStorage

--- src/utils/functions.util.js
+++ src/utils/functions.util.js
@@ -5,3 +5,19 @@ export const getAppTitle = () => {
 export const getAppDescription = () => {
     return process.env.REACT_APP_WEBSITE_DESCRIPTION;
 };
+
+export const saveUsersState = (state) => {
+    localStorage.setItem('users', JSON.stringify(state));
+}
+
+export const saveUserState = (state) => {
+    localStorage.setItem('user', JSON.stringify(state));
+}
+
+export const getUsersState = () => {
+    return JSON.parse(localStorage.getItem('users'));
+}
+
+export const getUserState = () => {
+    return JSON.parse(localStorage.getItem('user'));
+}
